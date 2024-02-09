67b5ee8ca4d93365f93c9b96a2deaed03966e4f4
Your Name <your.email@example.com>
2024-02-09 07:26:56 -0800

    first commit

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -26,25 +26,29 @@ export const Projects = () => {
     {
       title: "Shop Website",
       description: "Nextjs",
-      imgUrl: "Screenshot (9).png",
+      imgUrl:
+        "https://scontent.ftbs5-3.fna.fbcdn.net/v/t39.30808-6/426830869_3556458098001895_7717570636432169091_n.jpg?stp=dst-jpg_p180x540&_nc_cat=102&ccb=1-7&_nc_sid=c42490&_nc_ohc=i6yFkHgCd_IAX9Y7WOf&_nc_ht=scontent.ftbs5-3.fna&oh=00_AfBpa1m_xLH3grJGAcqO_fqYz8ij4C-bs30S-ttq1jzpBA&oe=65CA7345",
       webUrl: "https://shop-7l9b.vercel.app/",
     },
     {
       title: "Movie Website",
       description: "MERN",
       imgUrl: "Screenshot (13).png",
-      webUrl: "https://mov-dv4y.onrender.com/",
+      webUrl:
+        "https://scontent.ftbs5-3.fna.fbcdn.net/v/t39.30808-6/426920244_3556454141335624_7413064502428512203_n.jpg?stp=dst-jpg_p180x540&_nc_cat=105&ccb=1-7&_nc_sid=c42490&_nc_ohc=2nCupL2vaEIAX9EAVIL&_nc_ht=scontent.ftbs5-3.fna&oh=00_AfA88xyiOBXbkObzA4iX9mNuQ2W_XatFJerMCMdsrDmlew&oe=65CBD4E0",
     },
     {
       title: "Porfolio Website",
       description: "Nextjs",
-      imgUrl: "Screenshot (14).png",
+      imgUrl:
+        "https://scontent.ftbs5-2.fna.fbcdn.net/v/t39.30808-6/426870817_3556453858002319_2125734321908941033_n.jpg?stp=dst-jpg_s600x600&_nc_cat=110&ccb=1-7&_nc_sid=c42490&_nc_ohc=vtnWCdqbhFMAX_zrs1f&_nc_ht=scontent.ftbs5-2.fna&oh=00_AfC0gxsfQ5HmXJ-iKO13mg8I6h2Rl_0HtgQLTvrlBG9z8Q&oe=65CB9117",
       webUrl: "https://portfolio-lteg.vercel.app/",
     },
     {
       title: "Hoobank",
       description: "React",
-      imgUrl: "Screenshot (15).png",
+      imgUrl:
+        "https://scontent.ftbs5-2.fna.fbcdn.net/v/t39.30808-6/426600846_3556453831335655_5378008001873478697_n.jpg?stp=dst-jpg_s600x600&_nc_cat=104&ccb=1-7&_nc_sid=c42490&_nc_ohc=-uj5p2qHyNcAX_6Kv4o&_nc_oc=AQlO7tQ4AVS_nZq2DB-_3RgqXf6FZdY7SN5tO4kar_n2a6z2ABT1mkJdNrOA0MvOalE&_nc_ht=scontent.ftbs5-2.fna&oh=00_AfBPFCNts94Uq-lgDL5Cvo8wL3YZrQGZaHnW32Pan9skcQ&oe=65CB7C03",
       webUrl: "https://hoobankk.onrender.com/",
     },
   ];
